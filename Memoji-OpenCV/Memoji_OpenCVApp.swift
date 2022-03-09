275b8125c24358d0feede9d38404a8040217484b
oranthony <anthonyloroscio@gmail.com>
2022-03-09 18:54:48 +0100

    Initial Commit

---
+++ Memoji-OpenCV/Memoji_OpenCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memoji_OpenCVApp.swift
+//  Memoji-OpenCV
+//
+//  Created by anthony loroscio on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Memoji_OpenCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memoji-OpenCVUITests/Memoji_OpenCVUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Memoji_OpenCVUITestsLaunchTests.swift
+//  Memoji-OpenCVUITests
+//
+//  Created by anthony loroscio on 09/03/2022.
+//
+
+import XCTest
+
+class Memoji_OpenCVUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
