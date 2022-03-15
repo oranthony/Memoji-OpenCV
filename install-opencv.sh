ed8867b629a2a41a3b0c0a428e4d18efdf132534
oranthony <anthonyloroscio@gmail.com>
2022-03-15 14:50:24 +0100

    Update install script

--- install-opencv.sh
+++ install-opencv.sh
@@ -1,7 +1,7 @@
-curl -L https://drive.google.com/file/d/1A-MhC8euh_XYKqC5SF_EnjWueCxw89FA/view?usp=sharing --output opencv2_framework.zip
+curl -L https://sourceforge.net/projects/memoji-opencv/files/src/opencv2.framework.zip/download
 unzip -q opencv2_framework.zip
 rm opencv2_framework.zip
 
 
-curl -L https://drive.google.com/file/d/1daT0zKqn0AzwGYPHOWGDZ1q_GHSPlIUY/view?usp=sharing --output face-model.scn
+curl -L https://sourceforge.net/projects/memoji-opencv/files/src/face-model.zip/download --output face-model.scn
 mv face-model.scn Memoji-OpenCV/SceneKit Asset Catalog.scnassets
